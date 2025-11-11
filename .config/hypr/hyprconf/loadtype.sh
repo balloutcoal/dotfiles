f5d7f7812b39c70b509f821e56427dfb7b77ea08
balloutcoal <thorpcol000@gmail.com>
2025-11-11 15:10:49 -0700

    mac support

--- .config/hypr/hyprconf/loadtype.sh
+++ .config/hypr/hyprconf/loadtype.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -d /sys/class/power_supply/BAT0 ]; then
+if [ -d /sys/class/power_supply/BAT0 ] || [ -d /sys/class/power_supply/macsmc-battery ]; then
   cp ~/dotfiles/.config/hypr/hyprconf/laptop.conf ~/dotfiles/.config/hypr/hyprconf/primary.conf
 else
   cp ~/dotfiles/.config/hypr/hyprconf/desktop.conf ~/dotfiles/.config/hypr/hyprconf/primary.conf
